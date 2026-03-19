e93e4c10f393c553b25a1b742ebe265c773c2cd6
Mounir <lm_melzi@esi.dz>
2026-03-19 03:16:58 +0100

    fix path bug

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/pydi-framework/docs",
+  base: "/pydi-framework",
 });
